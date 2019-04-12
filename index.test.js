511aa1f298d23840755dcd15c835d243c851346d
Ratson <github@ratson.name>
2019-04-12 20:40:31 +0800

    Test HTML with content

--- index.test.js
+++ index.test.js
@@ -4,22 +4,24 @@ const posthtml = require('posthtml')
 
 const insert = require('.')
 
-const HTML = '<body></body>'
+const HTML = '<body><div id="app"></div><div /></body>'
 
 test('can insert content', async () => {
   const { html } = await posthtml([
     insert({ selector: 'body', content: '<div>test</div>' }),
   ]).process(HTML)
 
-  expect(html).toBe('<body><div>test</div></body>')
+  expect(html).toBe(
+    '<body><div id="app"></div><div></div><div>test</div></body>',
+  )
 })
 
 test('throw for missing selector', () => {
-  expect(() => insert({ content: '<div>test</div>' })).toThrowError(/selector/)
+  expect(() => insert({ content: '<div>test</div>' })).toThrow(/selector/)
 })
 
 test('throw for empty content', () => {
-  expect(() => insert({ selector: 'body', content: '' })).toThrowError(
-    /content/
+  expect(() => insert({ selector: 'body', content: '' })).toThrow(
+    /content/,
   )
 })
